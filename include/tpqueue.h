05413db8a07abce2d333b9a1cb5c0134a1692d87
ust-tanya <94698360+ust-tanya@users.noreply.github.com>
2022-05-20 23:25:13 +0300

    Update tpqueue.h

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -1,10 +1,82 @@
 // Copyright 2022 NNTU-CS
 #ifndef INCLUDE_TPQUEUE_H_
 #define INCLUDE_TPQUEUE_H_
+#include <string>
 
 template<typename T>
 class TPQueue {
-  // реализация шаблона очереди с приоритетом на связанном списке
+ private:
+  struct ITEM {
+  T data;
+  ITEM* prev, * next;
+  };
+  ITEM* head, * tail, * val;
+  TPQueue::ITEM* create(const T& data) {
+  ITEM* indx = new ITEM;
+  indx->data = data;
+  indx->next = nullptr;
+  indx->prev = nullptr;
+  return indx;
+  }
+
+ public:
+  TPQueue() {
+  head = tail = val = nullptr;
+  }
+  ~TPQueue() {
+  while (head)
+  pop();
+  }
+  T getNext() {
+  if (tail && head) {
+  if (!val)
+  val = head;
+  else if (val->next)
+  val = val->next;
+  else
+  throw std::string("Out error");
+  return val->data;
+  } else {
+  throw std::string("Out error");
+  }
+  }
+  T pop() {
+  if (tail && head) {
+  ITEM* temp = head->next;
+  if (temp)
+  temp->prev = nullptr;
+  T data = head->data;
+  delete head;
+  head = temp;
+  if (!head)
+  tail = nullptr;
+  return data;
+  } else {
+  throw std::string("Out error");
+  }
+  }
+  void push(const T& data) {
+  ITEM* temp = head;
+  ITEM* indx = create(data);
+  while (temp && temp->data.prior >= data.prior)
+  temp = temp->next;
+  if (!temp && head) {
+  tail->next = indx;
+  indx->prev = tail;
+  tail = indx;
+  } else if (!temp && !head) {
+  head = tail = indx;
+  } else if (!temp->prev) {
+  head->prev = indx;
+  indx->next = head;
+  head = indx;
+  } else {
+  temp->prev->next = indx;
+  indx->prev = temp->prev;
+  indx->next = temp;
+  temp->prev = indx;
+  }
+  }
 };
 
 struct SYM {
@@ -12,4 +84,4 @@ struct SYM {
   int prior;
 };
 
-#endif  // INCLUDE_TPQUEUE_H_
+#endif // INCLUDE_TPQUEUE_H_
